c67b944f0288c84ba47e8a9ac10ccf0a1b2db3e7
S1840085 <iamjerryphilpott@gmail.com>
2019-09-25 08:56:24 -0500

    Dabbling in OpenCV

--- Dabblin.py
+++ Dabblin.py
@@ -1,3 +1,6 @@
 import cv2
-moarjpeg = cv2.imread('messi5.jpg' , 1)
+moarjpeg = cv2.imread('messi5.jpg' , 0)
 print(moarjpeg)
+cv2.imshow('image', moarjpeg)
+cv2.waitKey(5000)
+cv2.imwrite('dark_mess.jpeg', moarjpeg)
