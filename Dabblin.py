0379f612822c6e3d42bf20d81479e3060259aa52
S1840085 <iamjerryphilpott@gmail.com>
2019-09-25 11:32:05 -0500

    Dabbling in OpenCV

--- Dabblin.py
+++ Dabblin.py
@@ -2,5 +2,7 @@ import cv2
 moarjpeg = cv2.imread('messi5.jpg' , 0)
 print(moarjpeg)
 cv2.imshow('image', moarjpeg)
-cv2.waitKey(5000)
+cv2.waitKey(2000)
 cv2.imwrite('dark_mess.jpeg', moarjpeg)
+nummies = [4, 5, 6, 82]
+print(nummies)
