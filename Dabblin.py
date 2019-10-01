efc0272363e5988c15dae555852e10bf71ac121f
S1840085 <iamjerryphilpott@gmail.com>
2019-10-01 09:24:03 -0500

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -1,9 +1,11 @@
 import cv2
 import tkinter
-moarjpeg = cv2.imread('messi5.jpg' , 0)
+import numpy
+moarjpeg = cv2.imread('messi5.jpg' , 1)
 print(moarjpeg)
 cv2.imshow('image', moarjpeg)
 cv2.waitKey(2000)
-cv2.imwrite('dark_mess.jpeg', moarjpeg)
+#cv2.imwrite('dark_mess.jpeg', moarjpeg)
 nummies = [4, 5, 6, 82]
 print(nummies)
+m = tkinter.Tk()
