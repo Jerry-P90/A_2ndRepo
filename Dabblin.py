9cd93562daecfb0ef8b31074fce403a45f6fd5c2
S1840085 <iamjerryphilpott@gmail.com>
2019-10-10 09:00:35 -0500

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -4,7 +4,15 @@ import numpy
 moarjpeg = cv2.imread('messi5.jpg')
 grayjpg = cv2.cvtColor(moarjpeg, cv2.COLOR_BGR2GRAY)
 newtemp = cv2.imread('messi_face.jpg' , 0)
+w, h = newtemp.shape[::-1]
+
 matched = cv2.matchTemplate(grayjpg, newtemp, cv2.TM_CCOEFF_NORMED)
+min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matched)
+
+top_left = max_loc
+bottom_right = (top_left[0] + w, top_left[1] + h)
+cv2.rectangle(moarjpeg,top_left, bottom_right, 255, 2)
+
 #print(moarjpeg)
 cv2.imshow('image', moarjpeg)
 cv2.waitKey(2000)
