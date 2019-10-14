7a7ef07310185f9b1e68b36e55d45be83cd141cd
S1840085 <iamjerryphilpott@gmail.com>
2019-10-14 08:43:04 -0500

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -1,6 +1,10 @@
 import cv2
 import tkinter
 import numpy
+from appJar import gui
+app = gui()
+app.addLabel("title", "Welcome to appJar")
+app.setLabelBg("title", "red")
 moarjpeg = cv2.imread('messi5.jpg')
 grayjpg = cv2.cvtColor(moarjpeg, cv2.COLOR_BGR2GRAY)
 newtemp = cv2.imread('messi_face.jpg' , 0)
@@ -15,8 +19,9 @@ cv2.rectangle(moarjpeg,top_left, bottom_right, 255, 2)
 
 #print(moarjpeg)
 cv2.imshow('image', moarjpeg)
-cv2.waitKey(2000)
+cv2.waitKey(0)
 print(matched)
+app.go()
 #cv2.imwrite('dark_mess.jpeg', moarjpeg)
 #nummies = [4, 5, 6, 82]
 #print(nummies)
