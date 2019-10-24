9a3e685370ab4945c905650688beb91a524af499
S1840085 <iamjerryphilpott@gmail.com>
2019-10-24 09:21:48 -0500

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -23,14 +23,14 @@ print(matched)
 #ORB Matching
 
 img1 = cv2.imread('brick_house.jpg', 0)
-img2 = cv2.imread('brick_wall.jpg', 0)
+img2 = cv2.imread('brick.jpg', 0)
 orb = cv2.ORB_create()
 kp1, desc1 = orb.detectAndCompute(img1,None)
 kp2, desc2 = orb.detectAndCompute(img2,None)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 matches = bf.match(desc1,desc2)
 matches = sorted(matches, key = lambda x:x.distance)
-img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:20],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 plt.imshow(img3),plt.show()
 
 #AppJar
