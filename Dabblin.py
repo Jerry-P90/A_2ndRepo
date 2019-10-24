57f0e141bb71475c90de85c8d6bf1c838729529d
S1840085 <iamjerryphilpott@gmail.com>
2019-10-24 08:59:58 -0500

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -1,5 +1,6 @@
 import cv2
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
 from appJar import gui
 import tkinter as Tk
 
@@ -18,6 +19,19 @@ cv2.imshow('image', moarjpeg)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
 print(matched)
+
+#ORB Matching
+
+img1 = cv2.imread('brick_house.jpg', 0)
+img2 = cv2.imread('brick_wall.jpg', 0)
+orb = cv2.ORB_create()
+kp1, desc1 = orb.detectAndCompute(img1,None)
+kp2, desc2 = orb.detectAndCompute(img2,None)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+matches = bf.match(desc1,desc2)
+matches = sorted(matches, key = lambda x:x.distance)
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(img3),plt.show()
 
 #AppJar
 
