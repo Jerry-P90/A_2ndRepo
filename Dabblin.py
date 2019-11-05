50009e6046cc4cf2a27f4368b956579d52fc8c73
S1840085 <iamjerryphilpott@gmail.com>
2019-11-05 08:29:49 -0600

    Learning how to do project

--- Dabblin.py
+++ Dabblin.py
@@ -14,7 +14,6 @@ min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(matched)
 top_left = max_loc
 bottom_right = (top_left[0] + w, top_left[1] + h)
 cv2.rectangle(moarjpeg,top_left, bottom_right, 255, 2)
-#print(moarjpeg)
 cv2.imshow('image', moarjpeg)
 cv2.waitKey(2000)
 cv2.destroyAllWindows()
@@ -38,18 +37,6 @@ app = gui("Login Window", "400x200")
 app.addLabel("title", "AppJar Testing")
 app.setBg("light blue")
 app.setLabelBg("title", "white")
-#app.addLabelEntry("Username")
-#app.addLabelSecretEntry("Password")
-#def press(button):
-#    if button == "Cancel":
-#        app.stop()
-#    else:
-#        usr = app.getEntry("Username")
-#        pwd = app.getEntry("Password")
-#        print("User:", usr, "Pass:", pwd)
-#app.addButtons(["Submit", "Cancel"], press)
-#app.addButton("Insert image", press)
-#app.setBgImage('think_spin.gif')
 def press(button):
     if button == "Match Images":
         moarjpeg = cv2.imread('messi5.jpg')
@@ -61,7 +48,6 @@ def press(button):
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
         cv2.rectangle(moarjpeg, top_left, bottom_right, 255, 2)
-        # print(moarjpeg)
         cv2.imshow('image', moarjpeg)
         cv2.waitKey(2000)
         cv2.destroyAllWindows()
