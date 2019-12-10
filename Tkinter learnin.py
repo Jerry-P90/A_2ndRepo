074816321809194f63c176294786c138e48011bc
S1840085 <iamjerryphilpott@gmail.com>
2019-12-10 09:38:10 -0600

    Learning how to do project

--- Tkinter learnin.py
+++ Tkinter learnin.py
@@ -1,14 +1,22 @@
 from tkinter import *
 
 root = Tk()
-myLabel = Label(root, text="I am the Lorfecks")
+root.title('Image Matching')
+#root.iconbitmap('C:/Users/S1840085/Pictures/Rando trash/Icon.ico')
+
+def myClick():
+    myLabel = Label(root, text="You clicked a button, nice.")
+    myLabel.pack()
+
+#myLabel = Label(root, text="I am the Lorfecks")
 myLabel2 = Label(root, text="and I speek for the trez")
-myButton = Button(root , text="Click me, now", padx=100)
+myButton = Button(root , text="Click me, now", padx=100, command= myClick)
 
-myLabel.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
-myButton.grid(row=2, column=0)
-
-#def myClick():
+#myLabel.grid(row=0, column=0)
+#myLabel2.grid(row=1, column=0)
+#myButton.grid(row=2, column=0)
+#myLabel.pack()
+myLabel2.pack()
+myButton.pack()
 
 root.mainloop()
